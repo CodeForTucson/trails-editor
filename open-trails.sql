fd426b47fbab0eda01be93d19488da5407dd3813
asonnenschein <adrian.sonnenschein@gmail.com>
2015-06-16 20:11:27 -0700

    start on users routes

--- open-trails.sql
+++ open-trails.sql
@@ -1,5 +1,14 @@
+CREATE TABLE users (
+    users_id SERIAL PRIMARY KEY,
+    username VARCHAR,
+    email VARCHAR,
+    password VARCHAR,
+    created_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE stewards (
     stewards_id SERIAL PRIMARY KEY,
+    users_id INTEGER REFERENCES users(users_id),
     name VARCHAR,
     url VARCHAR,
     phone VARCHAR,
@@ -10,8 +19,8 @@ CREATE TABLE stewards (
 
 CREATE TABLE trail_segments (
     trail_segments_id SERIAL PRIMARY KEY,
-    stewards_id INTEGER REFERENCES stewards(stewards_id)
-        ON DELETE CASCADE,
+    stewards_id INTEGER REFERENCES stewards(stewards_id) ON DELETE CASCADE,
+    users_id INTEGER REFERENCES users(users_id),
     motor_vehicles VARCHAR,
     foot VARCHAR,
     bicycle VARCHAR,
@@ -24,6 +33,7 @@ CREATE TABLE trail_segments (
 
 CREATE TABLE named_trails (
     named_trails_id SERIAL PRIMARY KEY,
+    users_id INTEGER REFERENCES users(users_id),
     name VARCHAR,
     trail_segments_id INTEGER REFERENCES trail_segments(trail_segments_id)
         ON DELETE CASCADE,
@@ -33,6 +43,7 @@ CREATE TABLE named_trails (
 
 CREATE TABLE trailheads (
     trailheads_id SERIAL PRIMARY KEY,
+    users_id INTEGER REFERENCES users(users_id),
     name VARCHAR,
     trail_segments_id INTEGER REFERENCES trail_segments(trail_segments_id)
         ON DELETE CASCADE,
@@ -48,6 +59,7 @@ CREATE TABLE trailheads (
 
 CREATE TABLE areas (
     areas_id SERIAL PRIMARY KEY,
+    users_id INTEGER REFERENCES users(users_id),
     name VARCHAR,
     stewards_id INTEGER REFERENCES stewards(stewards_id) ON DELETE CASCADE,
     url VARCHAR,
@@ -55,6 +67,7 @@ CREATE TABLE areas (
     geometry JSON
 );
 
+ALTER TABLE users OWNER TO trailseditor_user;
 ALTER TABLE trail_segments OWNER TO trailseditor_user;
 ALTER TABLE named_trails OWNER TO trailseditor_user;
 ALTER TABLE trailheads OWNER TO trailseditor_user;
